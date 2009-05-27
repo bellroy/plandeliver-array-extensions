c471b0a9e897b6a12313c9ae4b129a4c5432495a
Thomas Sommer <ts@Ts-MacBook.local>
2009-05-27 14:57:30 +1000

    Included the combination method from Ruby 1.9

--- lib/array_extensions.rb
+++ lib/array_extensions.rb
@@ -1,3 +1,21 @@
 module ArrayExtensions
   def to_html_json; ActiveSupport::JSON.encode(self).gsub(/"/, '\'') end
+  
+  # combination method of Ruby 1.9
+  def combination(num)
+    return [] if num < 1 || num > size
+    return map{ |e| [e] } if num == 1
+    
+    tmp = self.dup
+    self[0, size - (num - 1)].inject([]) do |ret, e|
+      tmp.shift
+      ret += tmp.combination(num - 1).map{ |a| a.unshift(e) }
+    end
+  end
+  
+  # adds reverse entries for the combination, only works with pairs
+  def combination_plus_reverse
+    combs = combination(2)
+    combs + combs.map { |c| c.reverse } 
+  end
 end
--- spec/array_extensions_spec.rb
+++ spec/array_extensions_spec.rb
@@ -12,12 +12,30 @@ describe Array do
     end
     
     it "should call to_json" do
-      @array.should_receive(:to_json).and_return ""
+      ActiveSupport::JSON.should_receive(:encode).and_return ""
       @array.to_html_json
     end
     
     it "should replace all double with single quotes" do
-      @array.to_html_json.should == "['1,2', '2,1']"
+      @array.to_html_json.should == "['1,2','2,1']"
+    end
+  end
+  
+  describe "combination" do
+    it "should work for several arrays and parameters" do
+      [1,2,3,4].combination(0).should == []
+      [1,2,3,4].combination(1).should == [[1], [2], [3], [4]]
+      [1,2,3,4].combination(2).should == [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+      [1,2,3,4].combination(3).should == [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]]
+      [1,2,3,4].combination(4).should == [[1, 2, 3, 4]]
+      [1,2,3,4].combination(5).should == []
+    end
+  end
+  
+  describe "combination plus reverse" do
+    it "should work for any array" do
+      [1,2,3,4].combination_plus_reverse.should ==
+        [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4], [2, 1], [3, 1], [4, 1], [3, 2], [4, 2], [4, 3]]
     end
   end
 end
